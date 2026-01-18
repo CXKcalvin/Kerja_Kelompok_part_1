d35024fb52e8c8e1e35c6d7d4322cd921ec80442
CXK_calvin <calvinpradivta0201@gmail.com>
2026-01-18 23:55:03 +0700

    Sedikit Perbaiki Bagian Halaman Pesan

--- js/menu.js
+++ js/menu.js
@@ -3,36 +3,48 @@ const menus = [
     id: "pecel-lele",
     nama: "Pecel Lele",
     harga: 15000,
-    gambar: "img/pecel lele.webp"
+    hargavar: "15000",
+    gambar: "img/pecel lele.webp",
+    gambar2: "../img/pecel lele.webp"
   },
   {
     id: "seblak",
     nama: "Seblak",
     harga: 12000,
-    gambar: "img/seblak.webp"
+    hargavar: "12000",
+    gambar: "img/seblak.webp",
+    gambar2: "../img/seblak.webp"
   },
   {
     id: "bebek-bakar",
     nama: "Bebek Bakar",
     harga: 25000,
-    gambar: "img/bebek bakar.webp"
+    hargavar: "25000",
+    gambar: "img/bebek bakar.webp",
+    gambar2: "../img/bebek bakar.webp"
   },
   {
     id: "ayam-bakar",
     nama: "Ayam Bakar",
     harga: 20000,
-    gambar: "img/ayam bakar.webp"
+    hargavar: "20000",
+    gambar: "img/ayam bakar.webp",
+    gambar2: "../img/ayam bakar.webp"
   },
   {
     id: "es-teh",
     nama: "Es Teh",
     harga: 5000,
-    gambar: "img/es teh.jpg"
+    hargavar: "5000",
+    gambar: "img/es teh.jpg",
+    gambar2: "../img/es teh.jpg"
   },
   {
     id: "pop-ice",
     nama: "Pop Ice",
     harga: 7000,
-    gambar: "img/pop ice.jpeg"
+    hargavar: "7000",
+    gambar: "img/pop ice.jpeg",
+    gambar2: "../img/pop ice.jpeg"
   }
 ];
