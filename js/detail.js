d1bb2672e49b7db09431c62322e782d27779a9ed
CXK_calvin <calvinpradivta0201@gmail.com>
2026-01-20 23:49:53 +0700

    Perbaiki Bagian Pesan Img Gak Muncul

--- js/detail.js
+++ js/detail.js
@@ -2,9 +2,12 @@ const params = new URLSearchParams(window.location.search);
 const menuId = params.get("menu");
 
 const menu = menus.find(m => m.id === menuId);
+const menu_img = menus.filter(m => m.gambar2 === menuId);
 
 if (menu) {
   document.getElementById("nama").textContent = menu.nama;
-  document.getElementById("gambar").src = menu.gambar;
   document.getElementById("harga").textContent = "Harga: Rp " + menu.harga;
+}
+if (menu_img) {
+  document.getElementById("gambar").src = menu.gambar2;
 }
