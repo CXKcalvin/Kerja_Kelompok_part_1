fcf5d1d2386519d69889c115b3c33b0bd621315d
CXK_calvin <calvinpradivta0201@gmail.com>
2026-01-22 22:03:23 +0700

    menambahkan input bayar

--- js/pesan.js
+++ js/pesan.js
@@ -1,10 +1,22 @@
+const ck_porsi = document.getElementById('cek-porsi');
 const ck_harga = document.getElementById('cek-harga');
+const total_harga = document.getElementById('harga');
 
 
 function dicek() {
-    if (ck_harga === 0 || ck_harga.value === '') {
-        alert('Mohon masukkan jumlah pesanan Anda!');
-    } else {
-        window.location.href = '../html/pesan.html';
-    }
+    if (ck_harga=== "") {
+            alert('Mohon masukkan total harga pesanan Anda!');
+        }
+        else if (ck_porsi === 0 || ck_porsi=== ''){
+            alert('Mohon masukkan jumlah pesanan Anda!');
+        }
+        else if (ck_harga< total_harga){
+            alert('Total harga yang Anda masukkan kurang dari harga menu!');
+        }
+        else if (ck_harga> total_harga){
+            window.location.href = '../html/pesan.html';
+        }
+        else if (ck_harga== total_harga){
+            window.location.href = '../html/pesan.html';
+        }
 }
